def2a2e3110ca78fd0fb4b581635060e2245c27e
Alex Sacco <aesacco@knights.ucf.edu>
2023-02-09 01:14:15 -0800

    Add project files.

---
+++ MusicDatabaseApp/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lblArtist = New System.Windows.Forms.Label()
+        Me.lblSongName = New System.Windows.Forms.Label()
+        Me.lblAlbumName = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblGenre = New System.Windows.Forms.Label()
+        Me.txtArtist = New System.Windows.Forms.TextBox()
+        Me.txtSongName = New System.Windows.Forms.TextBox()
+        Me.txtAlbumName = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.lstGenre = New System.Windows.Forms.ListBox()
+        Me.dgvLoadedDb = New System.Windows.Forms.DataGridView()
+        Me.btnCloseApp = New System.Windows.Forms.Button()
+        Me.btnCreateNewDb = New System.Windows.Forms.Button()
+        Me.btnDeleteRecord = New System.Windows.Forms.Button()
+        CType(Me.dgvLoadedDb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(567, 243)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(114, 29)
+        Me.btnLoad.TabIndex = 0
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(567, 278)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(114, 29)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'lblArtist
+        '
+        Me.lblArtist.AutoSize = True
+        Me.lblArtist.Location = New System.Drawing.Point(63, 21)
+        Me.lblArtist.Name = "lblArtist"
+        Me.lblArtist.Size = New System.Drawing.Size(44, 20)
+        Me.lblArtist.TabIndex = 2
+        Me.lblArtist.Text = "Artist"
+        '
+        'lblSongName
+        '
+        Me.lblSongName.AutoSize = True
+        Me.lblSongName.Location = New System.Drawing.Point(63, 68)
+        Me.lblSongName.Name = "lblSongName"
+        Me.lblSongName.Size = New System.Drawing.Size(87, 20)
+        Me.lblSongName.TabIndex = 3
+        Me.lblSongName.Text = "Song Name"
+        '
+        'lblAlbumName
+        '
+        Me.lblAlbumName.AutoSize = True
+        Me.lblAlbumName.Location = New System.Drawing.Point(63, 113)
+        Me.lblAlbumName.Name = "lblAlbumName"
+        Me.lblAlbumName.Size = New System.Drawing.Size(97, 20)
+        Me.lblAlbumName.TabIndex = 4
+        Me.lblAlbumName.Text = "Album Name"
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Location = New System.Drawing.Point(63, 163)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(37, 20)
+        Me.lblYear.TabIndex = 5
+        Me.lblYear.Text = "Year"
+        '
+        'lblGenre
+        '
+        Me.lblGenre.AutoSize = True
+        Me.lblGenre.Location = New System.Drawing.Point(63, 227)
+        Me.lblGenre.Name = "lblGenre"
+        Me.lblGenre.Size = New System.Drawing.Size(48, 20)
+        Me.lblGenre.TabIndex = 6
+        Me.lblGenre.Text = "Genre"
+        '
+        'txtArtist
+        '
+        Me.txtArtist.Location = New System.Drawing.Point(170, 21)
+        Me.txtArtist.Name = "txtArtist"
+        Me.txtArtist.Size = New System.Drawing.Size(381, 27)
+        Me.txtArtist.TabIndex = 7
+        '
+        'txtSongName
+        '
+        Me.txtSongName.Location = New System.Drawing.Point(170, 68)
+        Me.txtSongName.Name = "txtSongName"
+        Me.txtSongName.Size = New System.Drawing.Size(381, 27)
+        Me.txtSongName.TabIndex = 8
+        '
+        'txtAlbumName
+        '
+        Me.txtAlbumName.Location = New System.Drawing.Point(170, 113)
+        Me.txtAlbumName.Name = "txtAlbumName"
+        Me.txtAlbumName.Size = New System.Drawing.Size(381, 27)
+        Me.txtAlbumName.TabIndex = 9
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(170, 163)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(381, 27)
+        Me.txtYear.TabIndex = 10
+        '
+        'lstGenre
+        '
+        Me.lstGenre.FormattingEnabled = True
+        Me.lstGenre.ItemHeight = 20
+        Me.lstGenre.Items.AddRange(New Object() {"Blues", "Country", "Easy Listening", "Electronic", "Contemporary Folk", "Hip Hop", "Jazz", "Pop", "R&B and Soul", "Rock", "Metal", "Punk", "Latin", "Religious", "Traditional Folk"})
+        Me.lstGenre.Location = New System.Drawing.Point(170, 214)
+        Me.lstGenre.Name = "lstGenre"
+        Me.lstGenre.Size = New System.Drawing.Size(381, 224)
+        Me.lstGenre.TabIndex = 11
+        '
+        'dgvLoadedDb
+        '
+        Me.dgvLoadedDb.AllowUserToAddRows = False
+        Me.dgvLoadedDb.AllowUserToDeleteRows = False
+        Me.dgvLoadedDb.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvLoadedDb.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvLoadedDb.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.dgvLoadedDb.Location = New System.Drawing.Point(691, 113)
+        Me.dgvLoadedDb.Name = "dgvLoadedDb"
+        Me.dgvLoadedDb.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
+        Me.dgvLoadedDb.RowTemplate.Height = 29
+        Me.dgvLoadedDb.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.dgvLoadedDb.Size = New System.Drawing.Size(679, 293)
+        Me.dgvLoadedDb.TabIndex = 12
+        '
+        'btnCloseApp
+        '
+        Me.btnCloseApp.Location = New System.Drawing.Point(567, 409)
+        Me.btnCloseApp.Name = "btnCloseApp"
+        Me.btnCloseApp.Size = New System.Drawing.Size(114, 29)
+        Me.btnCloseApp.TabIndex = 13
+        Me.btnCloseApp.Text = "Close App"
+        Me.btnCloseApp.UseVisualStyleBackColor = True
+        '
+        'btnCreateNewDb
+        '
+        Me.btnCreateNewDb.Location = New System.Drawing.Point(567, 313)
+        Me.btnCreateNewDb.Name = "btnCreateNewDb"
+        Me.btnCreateNewDb.Size = New System.Drawing.Size(114, 55)
+        Me.btnCreateNewDb.TabIndex = 14
+        Me.btnCreateNewDb.Text = "Create New Db"
+        Me.btnCreateNewDb.UseVisualStyleBackColor = True
+        '
+        'btnDeleteRecord
+        '
+        Me.btnDeleteRecord.Location = New System.Drawing.Point(567, 374)
+        Me.btnDeleteRecord.Name = "btnDeleteRecord"
+        Me.btnDeleteRecord.Size = New System.Drawing.Size(114, 29)
+        Me.btnDeleteRecord.TabIndex = 15
+        Me.btnDeleteRecord.Text = "Delete Record"
+        Me.btnDeleteRecord.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1393, 450)
+        Me.Controls.Add(Me.btnDeleteRecord)
+        Me.Controls.Add(Me.btnCreateNewDb)
+        Me.Controls.Add(Me.btnCloseApp)
+        Me.Controls.Add(Me.dgvLoadedDb)
+        Me.Controls.Add(Me.lstGenre)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtAlbumName)
+        Me.Controls.Add(Me.txtSongName)
+        Me.Controls.Add(Me.txtArtist)
+        Me.Controls.Add(Me.lblGenre)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.lblAlbumName)
+        Me.Controls.Add(Me.lblSongName)
+        Me.Controls.Add(Me.lblArtist)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Name = "Form1"
+        Me.Text = "Alex's Music Database Application"
+        CType(Me.dgvLoadedDb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents lblArtist As Label
+    Friend WithEvents lblSongName As Label
+    Friend WithEvents lblAlbumName As Label
+    Friend WithEvents lblYear As Label
+    Friend WithEvents lblGenre As Label
+    Friend WithEvents txtArtist As TextBox
+    Friend WithEvents txtSongName As TextBox
+    Friend WithEvents txtAlbumName As TextBox
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents lstGenre As ListBox
+    Friend WithEvents dgvLoadedDb As DataGridView
+    Friend WithEvents btnCloseApp As Button
+    Friend WithEvents btnCreateNewDb As Button
+    Friend WithEvents btnDeleteRecord As Button
+End Class
---
+++ MusicDatabaseApp/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' ID variable used in Updating And Deleting Record  
+    Dim intID As Integer = 0
+
+    ' global filename variable
+    Dim stFileName As String
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim artist As String = txtArtist.Text
+        Dim songName As String = txtSongName.Text
+        Dim albumName As String = txtAlbumName.Text
+        Dim year As String = txtYear.Text
+        Dim genre As String = lstGenre.SelectedItem
+
+
+        Dim row As String = artist & "," & songName & "," & albumName & "," & year & "," & genre & Environment.NewLine
+
+        If IsNothing(stFileName) Then
+            Dim stDialogTitle As String = "Please Select a CSV File"
+            Dim stInitDirectory As String = "C:\Users\Public\Documents"
+            Dim fd As OpenFileDialog = New OpenFileDialog()
+
+            stDialogTitle = fd.Title()
+            stInitDirectory = fd.InitialDirectory()
+            fd.Filter = "CSV Files (*.csv)|*.csv"
+            fd.FilterIndex = 2
+            fd.RestoreDirectory = True
+
+            If fd.ShowDialog() = DialogResult.OK Then
+                stFileName = fd.FileName
+                File.AppendAllText(stFileName, row)
+
+                txtArtist.Text = ""
+                txtSongName.Text = ""
+                txtAlbumName.Text = ""
+                txtYear.Text = ""
+                lstGenre.ClearSelected()
+
+                dgvLoadedDb.DataSource = Nothing 'clear table
+                dgvLoadedDb.Rows.Clear()
+                Dim TextLine As String = ""
+                Dim objReader As New System.IO.StreamReader(stFileName)
+                Dim lines As List(Of String) = File.ReadAllLines(fd.FileName).ToList()
+                fill_Columns(lines)
+                For i As Integer = 1 To lines.Count - 1
+                    Dim data As String() = lines(i).Split(",")
+                    addElements(
+                data(0),
+                data(1),
+                data(2),
+                data(3),
+                data(4)
+            ) 'All this elements has to match with the -fill_Columns- elements
+                Next
+                objReader.Close()
+            End If
+            MessageBox.Show("Data loaded successfully!")
+        Else
+            File.AppendAllText(stFileName, row)
+
+            txtArtist.Text = ""
+            txtSongName.Text = ""
+            txtAlbumName.Text = ""
+            txtYear.Text = ""
+            lstGenre.ClearSelected()
+
+            dgvLoadedDb.DataSource = Nothing 'clear table
+            dgvLoadedDb.Rows.Clear()
+            Dim TextLine As String = ""
+            Dim objReader As New System.IO.StreamReader(stFileName)
+            Dim lines As List(Of String) = File.ReadAllLines(stFileName).ToList()
+            fill_Columns(lines)
+            For i As Integer = 1 To lines.Count - 1
+                Dim data As String() = lines(i).Split(",")
+                addElements(
+            data(0),
+            data(1),
+            data(2),
+            data(3),
+            data(4)
+        ) 'All this elements has to match with the -fill_Columns- elements
+            Next
+            objReader.Close()
+        End If
+
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Dim stDialogTitle As String = "Please Select a CSV File"
+        Dim stInitDirectory As String = "C:\Users\Public\Documents"
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        stDialogTitle = fd.Title()
+        stInitDirectory = fd.InitialDirectory()
+        fd.Filter = "CSV Files (*.csv)|*.csv"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+
+            dgvLoadedDb.DataSource = Nothing 'clear table
+            dgvLoadedDb.Rows.Clear()
+            stFileName = fd.FileName
+            Dim TextLine As String = ""
+            Dim objReader As New System.IO.StreamReader(stFileName)
+            Dim lines As List(Of String) = File.ReadAllLines(fd.FileName).ToList()
+            fill_Columns(lines)
+            For i As Integer = 1 To lines.Count - 1
+                Dim data As String() = lines(i).Split(",")
+                addElements(
+                data(0),
+                data(1),
+                data(2),
+                data(3),
+                data(4)
+            ) 'All this elements has to match with the -fill_Columns- elements
+            Next
+            objReader.Close()
+            MessageBox.Show("Data loaded successfully!")
+
+        End If
+
+    End Sub
+
+    Private Sub fill_Columns(lines As List(Of String))
+        Dim columns() As String = lines(0).Split(",")
+        For Each item In columns
+            Dim col As New DataGridViewTextBoxColumn
+            col.HeaderText = item
+            col.Name = item
+            col.DataPropertyName = item
+            dgvLoadedDb.Columns.Add(col)
+        Next
+    End Sub
+
+    Private Sub addElements(Artist As String, SongName As String, ALbumName As String, Year As String, Genre As String)
+        dgvLoadedDb.Rows.Add(
+            Artist,
+            SongName,
+            ALbumName,
+            Year,
+            Genre
+        ) 'You add or delete as you need
+    End Sub
+
+    Private Sub btnCloseApp_Click(sender As Object, e As EventArgs) Handles btnCloseApp.Click
+        System.Windows.Forms.Application.Exit()
+
+    End Sub
+
+    Private Sub btnDeleteRecord_Click(sender As Object, e As EventArgs) Handles btnDeleteRecord.Click
+        If dgvLoadedDb.SelectedRows.Count > 0 Then
+
+            dgvLoadedDb.Rows.RemoveAt(dgvLoadedDb.CurrentRow.Index)
+            Dim headers = (From header As DataGridViewColumn In dgvLoadedDb.Columns.Cast(Of DataGridViewColumn)()
+                           Select header.HeaderText).ToArray
+            Dim rows = From row As DataGridViewRow In dgvLoadedDb.Rows.Cast(Of DataGridViewRow)()
+                       Where Not row.IsNewRow
+                       Select Array.ConvertAll(row.Cells.Cast(Of DataGridViewCell).ToArray, Function(c) If(c.Value IsNot Nothing, c.Value.ToString, ""))
+            Using sw As New IO.StreamWriter(stFileName)
+                sw.WriteLine(String.Join(",", headers))
+                For Each r In rows
+                    sw.WriteLine(String.Join(",", r))
+                Next
+            End Using
+
+
+        Else
+
+            MessageBox.Show("You must select a row")
+
+        End If
+    End Sub
+
+    Private Sub btnCreateNewDb_Click(sender As Object, e As EventArgs) Handles btnCreateNewDb.Click
+        Dim stInitDirectory As String = "C:\Users\Public\Documents"
+        Dim fullPath As String = stInitDirectory & "\" & "music_db.csv"
+
+
+        If File.Exists(fullPath) Then
+
+            MsgBox("File music_db.csv already exists and will not be overwritten.")
+        Else
+            'Create or overwrite the file.
+            Dim fs As FileStream = File.Create(fullPath)
+            fs.Close()
+
+            'Add headers to the file
+            Dim row As String = "Artist" & "," & "Song Name" & "," & "Album Name" & "," & "Year" & "," & "Genre" & Environment.NewLine
+            File.AppendAllText(fullPath, row)
+        End If
+
+    End Sub
+End Class
